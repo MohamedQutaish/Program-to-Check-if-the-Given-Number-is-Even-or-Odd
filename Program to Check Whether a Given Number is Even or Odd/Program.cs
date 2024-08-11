88a08c734c97adc4e3efceaa430938f872ed1629
mohamed qutaish <mooohmedshafie1234@gmail.com>
2024-08-11 18:52:57 -0400

    Add project files.

---
+++ Program to Check Whether a Given Number is Even or Odd/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_to_Check_Whether_a_Given_Number_is_Even_or_Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i;
+
+            Console.Write("Enter any number:");
+             i = int.Parse(Console.ReadLine());
+
+            if (i % 2 == 0)
+
+            {
+                Console.Write("Entered number is Even");
+                Console.Read();
+            }
+            
+            else
+            
+                {
+                Console.Write("Entered nubmer is Odd");
+                Console.Read();
+            }
+            
+
+        }
+
+
+
+
+
+
+
+    }
+}
